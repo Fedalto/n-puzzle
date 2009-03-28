9430650249aca4832509b66e3a8505b80e8847fa
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2009-03-28 18:31:17 -0300

    Proposta de modelo para o "solver",
    Nada implementado ainda. So' uma ideia.

---
+++ solver.py
@@ -0,0 +1,17 @@
+class Solver (object):
+    heuristica = None
+    lista_nodos = []
+
+    def ordena_lista (self):
+        pass
+
+    def prox_nodo (self):
+        pass
+
+    def gera_filhos (self, nodo):
+        # -> faz copias dos nodos
+        # -> aplica os movimentos as nodos (direita, esquerda,
+        # acima, abaixo).
+        # -> Recalcula o peso do nodo.
+        # -> Insere filhos na lista
+        pass
