a981a14cb8825acfba4d3284d24f4b43667ca4b1
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2009-03-28 17:02:19 -0300

    Modificacoes na inicializaçao do Tabuleiro
    Tabuleiro deve receber uma lista de listas prontas.
    Nao confere para ver se e' quadrado.
    
    Para tabuleiro aleatorios usa random_tab(n).

--- tabuleiro.py
+++ tabuleiro.py
@@ -10,17 +10,12 @@ class Tabuleiro (object):
     O elemento com valor igual a 0 (zero)
     representa o espaco em vazio.'''
 
-    def __init__ (self, n = 4):
-        ''' Cria um tabuleiro aleatorio de NxN '''
-        self.n = n
+    def __init__ (self, lista):
+        ''' Recebe uma lista de listas e
+        retorna um objeto Tabuleiro. '''
+        self.n = len(lista)
         self.size = self.n**2
-
-        l = [i for i in range(self.size)]
-        random.shuffle(l)
-
-        self.__tab = []
-        for i in range(0,self.size, n):
-            self.__tab.append( l[i:i+n] )
+        self.__tab = lista
 
     def __str__ (self):
         s = ''
@@ -31,11 +26,26 @@ class Tabuleiro (object):
     def __getitem__ (self, item):
         return self.__tab.__getitem__(item)
 
+    def copy (self):
+        return Tabuleiro(self.__tab)
+
+def random_tab (n = 4):
+    ''' Cria um tabuleiro aleatorio de NxN '''
+    size = n**2
+    l = [i for i in range(size)]
+    random.shuffle(l)
+
+    tab = []
+    for i in range(0,size, n):
+        tab.append( l[i:i+n] )
+
+    return Tabuleiro(tab)
 
 
 def main ():
-    t = Tabuleiro(4)
+    t = random_tab()
     print t
+    print t.copy()
 
 if __name__ == '__main__':
     main()
