bd1cf964c5b286972b3fa08db050f3f8bcd98168
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2009-03-28 12:21:59 -0300

    Versao com o tabuleiro sendo implementado como lista de listas. Ha' apenas o str, getitem, init.

--- tabuleiro.py
+++ tabuleiro.py
@@ -1,18 +1,34 @@
 #!/bin/env python
 
-# Python library
-import numpy
 
+# Python library
+import random
 
 class Tabuleiro (object):
+    ''' Classe que representa o tabuleiro.
+    E' implementado como uma lista de listas. 
+    O elemento com valor igual a 0 (zero)
+    representa o espaco em vazio.'''
 
-    def __init__ (self, dimensao = 4):
-        self.n = dimensao**2
-        self.tab = numpy.random.permutation(self.n)
-        self.tab = self.tab.reshape(dimensao,dimensao)
+    def __init__ (self, n = 4):
+        ''' Cria um tabuleiro aleatorio de NxN '''
+        self.size = n**2
+
+        l = [i for i in range(self.size)]
+        random.shuffle(l)
+
+        self.__tab = []
+        for i in range(0,self.size, n):
+            self.__tab.append( l[i:i+n] )
 
     def __str__ (self):
-        return self.tab.__str__()
+        s = ''
+        for line in self.__tab:
+            s += str(line) + '\n'
+        return s.rstrip()
+
+    def __getitem__ (self, item):
+        return self.__tab.__getitem__(item)
 
 
 
