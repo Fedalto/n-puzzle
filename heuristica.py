19b5587ab041b24162f7f5f51c5577bd6a64a187
diego <diego@forrest.(none)>
2009-03-29 19:43:26 -0300

    heuristica.py agora funcionando =)

--- heuristica.py
+++ heuristica.py
@@ -1,24 +1,25 @@
-class heuristica(object):
-    def __init__(self, tab):
-        self.tab = tab
-        self.matriz_multi = self._gera_matriz_multi()
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
+class heuristica(object):
+    def __init__(self, n):
+        self.n = n
+        self.matriz_multi = self._gera_matriz_multi()
 
     def _lde(self, x):
         """ Linha que X deveria estar """
-        return x-1%self.tab.n
+        return (x-1)/self.n
 
     def _cde(self, x):
         """ Coluna que X deveria estar """
-        return x-1/self.tab.n
+        return (x-1)%self.n
 
     def _peso(self, x, i, j):
-        return abs(self._lde(x) - 1) + abs(self._cde(x) - j)
+        return abs(self._lde(x) - i) + abs(self._cde(x) - j)
 
-    def calcula(self, no_pai, jogada=None):
+    def calcula(self, tab, peso_pai=None, jogada=None):
         """ Calcula peso do tabuleiro inteiro ou a diferença dele
             para uma jogada """
-        tab = no_pai.tab
 
         if jogada != None:
             tava = jogada.ondetava
@@ -26,7 +27,7 @@ class heuristica(object):
             valor = tab[foi[0],foi[1]]
             peso_velho = self._peso(valor,tava[0],tava[1])
             peso_novo = self._peso(valor,foi[0],foi[1])
-            return no_pai.peso - peso_velho-peso_novo
+            return peso_pai - peso_velho-peso_novo
 
         peso = 0
         for i in range(tab.n):
@@ -38,12 +39,12 @@ class heuristica(object):
         return peso
 
     def _h(self,x):
-        n = self.tab.n
+        n = self.n
         if x <= n: return n-1
         else: return (n-1)-((x-1)%n)
 
     def _gera_matriz_multi(self):
-        n = self.tab.n
+        n = self.n
         tab_multi = [[None]*n for i in range(n)]
 
         # FUNCIONA
