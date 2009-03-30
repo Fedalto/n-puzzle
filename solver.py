8cfe69aa802ab481106a8e9ac3ec4c784b33e9e0
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2009-03-30 20:35:31 -0300

    Implementado o Solver.
    Mas ainda com alguns problemas.

--- solver.py
+++ solver.py
@@ -3,26 +3,31 @@
 
 import tabuleiro
 from heuristica import heuristica
-from nodo import nodo
+from nodo import Nodo, movimento
 
 class Solver (object):
     def __init__(self, n=3, tabini=None):
-        self.heuristica = heuristica()
+        self.heuristica = heuristica(n)
         self.lista_nodos = []
         self.n = n
-        self.num_de_jogadas = 1
+        self.hash_pais = {}
+        self.hash_tab = {}
 
         # Se usuário não forçou um tabuleiro inicial, cria um aleatório
         if not tabini:
             tabini = tabuleiro.random_tab(self.n)
-
-        tabini = tabuleiro.Tabuleiro(tabini)
+        else:
+            tabini = tabuleiro.Tabuleiro(tabini)
 
         # Calcula peso do primeiro tabuleiro
-        peso = self.heuristica.calcula(tabini.tab)
+        peso = self.heuristica.calcula(tabini)
+
+        # Adiciona o tabuleiro na lista de hashs
+        self.hash_tab[hash(str(tabini.get_tab()))] = None
 
         # Cria um nodo
-        nodoini = nodo(tabini,None,peso,None,1)
+        nodoini = Nodo(tabini, 0, None,peso,None,0)
+        self.hash_pais[0] = nodoini
 
         # Adiciona nodo a lista de nodos
         self.adiciona_nodo(nodoini)
@@ -31,18 +36,75 @@ class Solver (object):
         """ Cada item da lista possui [peso,nodo] """
 
         self.lista_nodos.append([nodo.peso,nodo])
+        self.hash_pais[nodo.id] = nodo
         self.ordena_lista_nodos()
 
-    def ordena_lista(self):
+    def ordena_lista_nodos(self):
         self.lista_nodos.sort(cmp=lambda x,y: x[0] - y[0])
 
     def prox_nodo (self):
-        pass
+        return self.lista_nodos.pop(0)[1]
 
     def gera_filhos (self, nodo):
+        for nome_mov, mov in movimento.items():
+            novo_tab = nodo.tab.copy()
+            if not novo_tab.swap(mov):
+                continue
+            
+            if str(novo_tab.get_tab()) in self.hash_tab:
+                continue
+            self.hash_tab[hash(str(novo_tab.get_tab()))] = None
+            
+            # Jogada INVERTIDA
+            # Tudo e' relativo ao observador
+            jogada = {'ondetava': novo_tab.zero_pos,
+                      'ondefoi' : nodo.tab.zero_pos }
+
+            delta = self.heuristica.calcula(novo_tab, nodo.peso, jogada)
+            novo_nodo = Nodo(novo_tab, len(self.hash_pais) ,nodo.id,
+                             nodo.peso + delta +1, nome_mov, nodo.altura+1)
+
+            self.adiciona_nodo(novo_nodo)
+            print novo_nodo.peso
+            print novo_nodo.tab
+            print 
+
+
         # -> faz copias dos nodos
         # -> aplica os movimentos as nodos (right, left,
         #    up, down)
         # -> Recalcula o peso do nodo.
         # -> Insere filhos na lista
-        pass
+
+    def magic (self):
+        nodo = self.prox_nodo()
+        while nodo.peso - nodo.altura:
+###             print nodo.peso
+###             print nodo.tab
+###             print 
+            self.gera_filhos(nodo)
+            nodo = self.prox_nodo()
+            self.gera_filhos(nodo)
+            import sys
+            sys.exit()
+        self.print_solucao(nodo)
+
+
+    def print_solucao (self, nodo):
+        ###
+        #
+        # Aki faz tratamento de subida dos nodos, empilhamento dos movimentos
+        # e impressao do resultado, alem do cafe.
+        solution = []
+        
+        while nodo.id_pai:
+
+            solution.append(nodo)
+            nodo = self.hash_pais[nodo.id_pai]
+        solution.append(nodo)
+        
+        solution.reverse()
+
+        print '\n\nSOLUCAO\n'
+        for step in solution:
+            print step.tab
