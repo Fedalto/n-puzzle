c7231f0fe988ac03b63f191fea89649baec5bd29
Leonardo S. P. Fedalto <lfedalto@gmail.com>
2009-03-30 20:34:26 -0300

    find, swap, copy e get_ta em Tabuleiro.
    Adicionado métodos find(acha posição do elemento no tabuleiro);
    swap (troca 2 elementos de posição);
    get_tab (retorna a lista de listas, i.e., o tabuleiro);
    método copy foi concertado. Faz um deepcopy do objeto.
    A classe Tabuleiro guarda a posição do elemento 0 (zero).

--- tabuleiro.py
+++ tabuleiro.py
@@ -3,6 +3,7 @@
 
 # Python library
 import random
+import copy
 
 class Tabuleiro (object):
     ''' Classe que representa o tabuleiro.
@@ -16,6 +17,7 @@ class Tabuleiro (object):
         self.n = len(lista)
         self.size = self.n**2
         self.__tab = lista
+        self.zero_pos = self.find(0)
 
     def __str__ (self):
         s = ''
@@ -26,8 +28,30 @@ class Tabuleiro (object):
     def __getitem__ (self, item):
         return self.__tab.__getitem__(item)
 
+    def find (self, num):
+        for line in self.__tab:
+            if num in line:
+                break
+
+        # Retorna (linha,coluna) do elemento 0
+        return (self.__tab.index(line), line.index(num))
+
+    def swap (self, (x, y) ):
+        a, b = self.zero_pos
+#         print a, b, x, y
+        if a+x < 0 or b+y < 0 or a+x > self.n-1 or b+y > self.n-1:
+#            print a, b, x, y
+            return None
+        self.__tab[a][b] = self.__tab[a+x][b+y]
+        self.__tab[a + x][b + y] = 0
+        self.zero_pos = (a + x, b + y)
+        return True #?
+    
+    def get_tab(self):
+        return self.__tab
+
     def copy (self):
-        return Tabuleiro(self.__tab)
+        return copy.deepcopy(self)
 
 def random_tab (n = 4):
     ''' Cria um tabuleiro aleatorio de NxN '''
