213dcfe394592620e4fb986b73cfa701fbd92f7d
diego <diego@forrest.(none)>
2009-03-29 21:30:39 -0300

    solver.py: definição de __init__ e outros. Acho que me confundi :(

--- solver.py
+++ solver.py
@@ -1,9 +1,37 @@
-class Solver (object):
-    heuristica = None
-    lista_nodos = []
+import tabuleiro
+from heuristica import heuristica
+from nodo import nodo
 
-    def ordena_lista (self):
-        pass
+class Solver (object):
+    def __init__(self, n=3, tabini=None):
+        self.heuristica = heuristica()
+        self.lista_nodos = []
+        self.n = n
+        self.num_de_jogadas = 1
+
+        # Se usuário não forçou um tabuleiro inicial, cria um aleatório
+        if not tabini:
+            tabini = tabuleiro.random_tab(self.n)
+
+        tabini = tabuleiro.Tabuleiro(tabini)
+
+        # Calcula peso do primeiro tabuleiro
+        peso = self.heuristica.calcula(tabini.tab)
+
+        # Cria um nodo
+        nodoini = nodo(tabini,None,peso,None,1)
+
+        # Adiciona nodo a lista de nodos
+        self.adiciona_nodo(nodoini)
+
+    def adiciona_nodo(self, nodo):
+        """ Cada item da lista possui [peso,nodo] """
+
+        self.lista_nodos.append([nodo.peso,nodo])
+        self.ordena_lista_nodos()
+
+    def ordena_lista(self):
+        self.lista_nodos.sort(cmp=lambda x,y: x[0] - y[0])
 
     def prox_nodo (self):
         pass
