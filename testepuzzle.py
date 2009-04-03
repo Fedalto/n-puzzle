cbde38350d9563c285c785997036af97a9e8fa7d
diego <diego@forrest.(none)>
2009-04-02 23:38:21 -0300

    testepuzzle.py: Usando cProfile para verificar desempenho

--- testepuzzle.py
+++ testepuzzle.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 
 from solver import Solver
-from tabuleiro import Tabuleiro
+from tabuleiro import Tabuleiro,random_tab
+import cProfile
 
-tab_teste = [[1,2,3],[0,4,6],[7,5,8]]
+tab_teste = [[1, 2, 7, 3],[5, 6, 4, 8],[9, 11, 0, 12],[13, 10, 14, 15]]
+#tab_teste = random_tab(4,50).get_tab()
 
-s = Solver(n=3,tabini=tab_teste)
+s = Solver(n=4,tabini=tab_teste)
 
-s.magic()
+cProfile.run('s.magic()')
